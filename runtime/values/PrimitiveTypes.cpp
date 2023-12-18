f306478a930590ad7c7eff033c9ecb9c1b409d74
Bartosz Sobina <bartosz.sobina44@gmail.com>
2023-12-18 20:14:15 +0100

    refactor: split types definitions to two files (primitive and complex)

---
+++ runtime/values/PrimitiveTypes.cpp
@@ -0,0 +1,69 @@
+#include "Values.h"
+
+
+RuntimeVal::RuntimeVal(ValueType type) : type(type) {};
+
+
+NullVal::NullVal() : RuntimeVal(ValueType::NullValue) {}
+
+
+std::string NullVal::toString() {
+    return value;
+}
+
+
+BooleanVal::BooleanVal(bool b) : RuntimeVal(ValueType::BooleanValue), value(b) {}
+
+
+BooleanVal::BooleanVal(BooleanVal& orginal) : RuntimeVal(ValueType::BooleanValue), value(orginal.value) {}
+
+
+std::string BooleanVal::toString() {
+    if (value) {
+        return "true";
+    }
+    return "false";
+}
+
+
+NumberVal::NumberVal(double n) : RuntimeVal(ValueType::NumberValue), value(n) {}
+
+
+NumberVal::NumberVal(const NumberVal& orginal) : RuntimeVal(ValueType::NumberValue), value(orginal.value) {}
+
+
+NumberVal::NumberVal(const NumericLiteral* numberLiteral) : RuntimeVal(ValueType::NumberValue), value(numberLiteral->value) {}
+
+
+std::string NumberVal::toString() {
+    int valueInt = value;
+    if ((value - valueInt) == 0) {
+      return std::to_string(valueInt);
+    }
+    return std::to_string(value);
+}
+
+
+ReturnValue::ReturnValue(RuntimeVal* val) : RuntimeVal(ValueType::ReturnValue), value(val) {}
+
+
+ReturnValue::ReturnValue(ReturnValue& orginal) : RuntimeVal(ValueType::ReturnValue), value(orginal.value) {}
+
+
+std::string ReturnValue::toString() {
+    return value->toString();
+}
+
+
+StringVal::StringVal(const std::string& str) : RuntimeVal(ValueType::StringValue), value(str) {}
+
+
+StringVal::StringVal(const StringVal& orginal) : RuntimeVal(ValueType::StringValue), value(orginal.value) {}
+
+
+StringVal::StringVal(const StrLiteral* strLiteral) : RuntimeVal(ValueType::StringValue), value(strLiteral->value) {}
+
+
+std::string StringVal::toString() {
+    return value;
+}
